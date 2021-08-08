45f940ce1b830df3e79a68cf6673d775e8ee28e7
kzhang01 <karinazhang1@gmail.com>
2021-08-08 16:52:47 -0400

    add timeline to experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,9 +1,61 @@
-import React from "react";
+import "../styles/experience.css"
+import { ReactComponent as WorkIcon } from "../assets/work.svg"
+import { ReactComponent as SchoolIcon } from "../assets/school.svg"
+
+import TimelineElements from "./TimelineElements"
+
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component"
+
+import "react-vertical-timeline-component/style.min.css"
 
 export default function Experience() {
-    return (
-        <section id="experience">
-            <p>Experience</p>
-        </section>
-    )
+  let workIconStyles = { background: "#06D6A0" }
+  let schoolIconStyles = { background: "#f9c74f" }
+
+  return (
+    <section id="experience">
+      <h1 className="title">Timeline</h1>
+      <VerticalTimeline>
+        {TimelineElements.map(element => {
+          let isWorkIcon = element.icon === "work"
+          let showButton =
+            element.buttonText !== undefined &&
+            element.buttonText !== null &&
+            element.buttonText !== ""
+
+          return (
+            <VerticalTimelineElement
+              key={element.key}
+              date={element.date}
+              dateClassName="date"
+              iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
+              icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+            >
+              <h3 className="vertical-timeline-element-title">
+                {element.title}
+              </h3>
+              <h5 className="vertical-timeline-element-subtitle">
+                {element.location}
+              </h5>
+              <div id="description">{element.description}</div>
+              {showButton && (
+                <a
+                  className={`button ${
+                    isWorkIcon ? "workButton" : "schoolButton"
+                  }
+`}
+                  href={element.buttonLink}
+                >
+                  {element.buttonText}
+                </a>
+              )}
+            </VerticalTimelineElement>
+          )
+        })}
+      </VerticalTimeline>
+    </section>
+  )
 }
---
+++ src/components/TimelineElements.js
@@ -0,0 +1,59 @@
+let TimelineElements = [
+    {
+        id: 1,
+        title: "Microsoft",
+        location: "New Technologist Intern",
+        description:
+        "Worked through the software development lifecycle under mentorship of senior staff at Microsoft. Developed a web application using React, Mapbox API, and the Environmental Protection Agency’s Envirofacts API",
+        buttonText: "Program Website",
+        buttonLink: "https://www.newtechnologists.com",
+        date: "Jun 2021 - Aug 2021",
+        icon: "work",
+    },
+    {
+        id: 2,
+        title: "Classrooms Without Borders",
+        location: "Software Developer",
+        description:
+        "Created a web application for students to submit assignments, browse a database of past assignments, and filter results using React and Firebase. Regularly communicated with client to ensure seamless project hand-off and satisfaction with product requirements",
+        buttonText: "See Live Website",
+        buttonLink: "https://on-this-day-cwb.herokuapp.com/",
+        date: "Apr 2021 - Jun 2021",
+        icon: "work",
+    },
+    {
+        id: 3,
+        title: "HKP Solutions",
+        location: "iOS Developer Intern",
+        description:
+        "Worked in agile environment to implement new features for the hotel housekeeping management system using SwiftUI. Collaborated with the backend team to integrate internally built APIs for user sign-in/sign-up. Designed user experience frameworks applicable to multiple screen sizes, including both iPad and iPhone.",
+        buttonText: "Company Website",
+        buttonLink: "https://www.linkedin.com/company/hkpsolutions/",
+        date: "Dec 2020 - May 2021",
+        icon: "work",
+    },
+    {
+        id: 4,
+        title: "New York University",
+        location: "New York, NY",
+        description:
+        "Majoring in Computer Science at the College of Arts and Sciences, minoring in Web Programming and Applications",
+        buttonText: "University Website",
+        buttonLink: "https://cs.nyu.edu/home/index.html",
+        date: "Sep 2019 - Dec 2022",
+        icon: "school",
+    },
+    {
+        id: 5,
+        title: "FRC Team 1403 Cougar Robotics",
+        location: "Design Captain",
+        description:
+        "Led a team of 17 members to prototype, design, and manufacture mechanical subsystems using Autodesk Inventor to develop CAD models and a CNC machine programmed using G-code to manufacture precision parts. Won first district event in 8 years, won Mid-Atlantic Regional Chairman’s Award, and qualified for World Championship",
+        buttonText: "Team Website",
+        buttonLink: "https://cougarrobotics.com",
+        date: "Dec 2016 - Jun 2019",
+        icon: "work",
+    },
+  ]
+  
+  export default TimelineElements
