1268308bae7ab29dfa198f51c03d94eb733dd397
kzhang01 <karinazhang1@gmail.com>
2021-08-08 01:17:40 -0400

    responsive header, will add hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,53 +1,89 @@
 import React from "react";
 import styled from 'styled-components';
 import logo from "../assets/logo.svg";
+import { device } from "../utils/breakpoints";
 
 export default function Header() {
     const Header = styled.section`
-        background: papayawhip;
-        width: "100%";
-        height: 3em;
-        padding: 16px 20px;
-        
+        min-height: 3em;
+        padding: 4px 32px;
+
+        -webkit-box-shadow: 0 8px 6px -6px #999;
+        -moz-box-shadow: 0 8px 6px -6px #999;
+        box-shadow: 0 8px 6px -6px #999;
+
         display: flex;
         justify-content: space-between;
         align-items: center;
-       
-        ul {
-            list-style-type: none;
-            display: flex;
-        }
-        li {
-            float: left;
-            padding: 0px 24px;
-        }
-        a:link, a:visited, a:active {
+        
+        a {
             color: black;
             text-decoration: none;
         }
-        a:hover {
-            color: red;
-            transition: color 1s;
+
+        @media ${device.mobileL} {
+            flex-direction: column;
         }
     `;
-    
+
+    const LogoContainer = styled.section`
+        display: flex;
+        align-items: center;
+        img {
+            margin-right: 16px;
+        } 
+    `;
+    const Links = styled.section`
+        width: 20vw;
+        display: flex;
+        justify-content: space-between;
+
+        @media ${device.mobileL} {
+            flex-direction: column;
+        }
+        @media ${device.laptop} {
+            width: 30vw;
+        }
+
+        a:link, a:visited, a:active {
+            color: black;
+            text-decoration: none;
+            position: relative;
+        }
+        a:hover {
+            color: #000;
+        }
+        a::before {
+            content: "";
+            position: absolute;
+            display: block;
+            width: 100%;
+            height: 2px;
+            bottom: 0;
+            left: 0;
+            background-color: #73eba7;
+            transform: scaleX(0);
+            transition: transform 0.3s ease;
+        }
+        a:hover::before {
+            transform: scaleX(1);
+        }
+    `;
+
     return (
         <Header>
-            <img src={logo} height="48px" width="48px" alt="logo"/>
-            <ul>
-                <li>
-                    <a href="#home">Home</a>
-                </li>
-                <li>
-                    <a href="#about">About</a>
-                </li>
-                <li>
-                    <a href="#experience">Work Experience</a>
-                </li>
-                <li>
-                    <a href="#projects">Projects</a>
-                </li>
-            </ul>
+            <a href="#home">
+                <LogoContainer>
+                    <img src={logo} height="48px" width="48px" alt="logo"/>
+                    <h2>Karina Zhang</h2>
+                </LogoContainer>
+            </a>
+            <Links>
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#experience">Work</a>
+                <a href="#projects">Projects</a>
+            </Links>
         </Header>
     )
 }
