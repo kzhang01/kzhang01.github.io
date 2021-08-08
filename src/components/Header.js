5d61153111c1287dc4c8930c0da2038e941d4d6b
kzhang01 <karinazhang1@gmail.com>
2021-08-07 21:55:07 -0400

    skeleton of website nav set up

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <a href="#home">Home</a>
+                </li>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#experience">Work Experience</a>
+                </li>
+                <li>
+                    <a href="#projects">Projects</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <section id="home">
+            <p>Hi, I'm Karina</p>
+        </section>
+    )
+}
