99cd292c1c0f80d1eeb01cb1ccb199e72dec8268
kzhang01 <karinazhang1@gmail.com>
2021-08-09 01:37:20 -0400

    update title and clean up code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { device } from "../utils/breakpoints";
-
 import me from "../assets/me.JPG";
-import email from "../assets/email.svg";
-import github from "../assets/github.svg";
-import linkedin from "../assets/linkedin.svg";
 
 export default function Home() {
   const Home = styled.section`
@@ -13,7 +9,7 @@ export default function Home() {
     margin: auto;
     height: 50vh;
     h1 {
-        margin-bottom: 0
+      margin-bottom: 0;
     }
     img {
       width: 50%;
@@ -23,34 +19,27 @@ export default function Home() {
         width: 100%;
       }
       h1 {
-          margin-top: 32px;
+        margin-top: 32px;
       }
     }
 
     @media ${device.mobileS} {
-        h1 {
-            font-size: 3em;
-        }
+      h1 {
+        font-size: 3em;
+      }
     }
   `;
 
-  const About = styled.section``;
-  const Image = styled.section`
-    width: 30vw;
-    height: auto;
-  `;
   return (
     <Home id="home">
-      <About>
-        <h1 className="title" style={{  }}>
-          Hey there! I'm Karina
-        </h1>
+      <div>
+        <h1 className="title">Hey there! I'm Karina</h1>
         <h2 style={{ fontWeight: "normal" }}>
           I love untangling problems, learning new technologies, and meeting new
           people
         </h2>
-      </About>
-      <img src={me} />
+      </div>
+      <img src={me} alt="me" />
     </Home>
   );
 }
